c76cdd9610b8d1a7e9d3ee439f183a700e51ef6d
Belle P <bellepx3@gmail.com>
2019-07-13 03:30:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  sum = 0
+  sum = starting_point
   source_array.each do |number|
     sum +=number 
   end
